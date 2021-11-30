86e204aedbbdca7e26ae8132b41d70c72df0e9d8
Mitskevich Igor <jimaltair1987@gmail.com>
2021-11-30 15:44:18 +0200

    add comments to ExamController

--- examinator-micr/src/main/java/com/example/examinatormicr/ExamController.java
+++ examinator-micr/src/main/java/com/example/examinatormicr/ExamController.java
@@ -24,16 +24,30 @@ public class ExamController {
     @Value("${exam.title}")
     private String title;
 
+//    POST http://localhost:8082/exam
+//    Content-Type: application/json
+//
+//    {
+//        "MATH": 5,
+//        "history": 2
+//    }
+
     @PostMapping("/exam")
     public Exam getExam(@RequestBody Map<String, Integer> spec) {
         List<Section> sections = spec.entrySet().stream()
                 .map(entry -> restTemplate.getForObject(getUrl(entry), Question[].class))
+                // результат выполнения restTemplate.getForObject(...) -> http://MATH/api/questions?amount=5
+                //                                                     -> http://history/api/questions?amount=2
                 .map(questions -> Section.builder().questions(List.of(questions)).build())
+                // в результате получаем Section с вопросами по математике - содержит List<Question> c 5-ю вопросами по математике
+                //                       Section c вопросами по истории - содержит List<Question> c 2-мя вопросами по истории
                 .collect(Collectors.toList());
+        // возвращаем объект Exam - вместе с заголовком и List<Section>, который содержит две секции вопросов - по математике и истории
         return Exam.builder().section(sections).title(title).build();
     }
 
     private String getUrl(Map.Entry<String, Integer> entry) {
+        //entry.getKey() = "MATH" или "history"
         return "http://" + entry.getKey() + "/api/questions?amount=" + entry.getValue();
     }
 }
